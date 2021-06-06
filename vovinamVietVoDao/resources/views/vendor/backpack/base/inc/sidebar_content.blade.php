e2bb745ecd05ec41a5185591420f06ae55ccfd30
MedAnisse <Med-anisse.belhadj@outlook.com>
2021-06-06 20:53:12 +0200

    version 1

--- vovinamVietVoDao/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ vovinamVietVoDao/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,9 +1,8 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('arbitre') }}'><i class='nav-icon la la-question'></i> Arbitres</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('arbitre') }}'><i class='nav-icon la la-user'></i> Arbitres</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('club') }}'><i class='nav-icon la la-question'></i> Clubs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('combat') }}'><i class='nav-icon la la-question'></i> Combats</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('combat') }}'><i class='nav-icon la la-users'></i> Combats</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('entraineur') }}'><i class='nav-icon la la-question'></i> Entraineurs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('joueur') }}'><i class='nav-icon la la-question'></i> Joueurs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('juge') }}'><i class='nav-icon la la-question'></i> Juges</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('salle') }}'><i class='nav-icon la la-question'></i> Salles</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('joueur') }}'><i class='nav-icon la la-male'></i> Joueurs</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('juge') }}'><i class='nav-icon la la-gavel'></i> Juges</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('salle') }}'><i class='nav-icon la la-square'></i> Salles</a></li>
